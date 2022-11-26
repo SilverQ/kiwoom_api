33240763b494427dccc6e5eea42971be83f2433d
SilverQ <scor9947@hanmail.net>
2022-11-26 14:00:43 +0900

    2'nd commit

--- kiwoom.py
+++ kiwoom.py
@@ -4,8 +4,8 @@ from PyQt5.QtWidgets import *
 from PyQt5.QAxContainer import *
 from PyQt5.QtCore import *
 import time
-import pandas as pd
-import sqlite3
+# import pandas as pd
+# import sqlite3
 import telegram
 
 # https://kminito.tistory.com/36
